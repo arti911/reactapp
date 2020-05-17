9c1e5560012b4b8e34565fe9cdb2d7b219c4f088
Artur Papikian <artur.jog@yandex.ru>
2020-05-18 00:18:58 +0300

    Updates

---
+++ precache-manifest.6f1ff101871f7415014687d0cc0599f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d8074554284554101a51c46b72c96f",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "5224417a77d410517d2c",
+    "url": "/reactapp/static/css/main.d83c3134.chunk.css"
+  },
+  {
+    "revision": "fa22a71a492cf4a0c197",
+    "url": "/reactapp/static/js/2.1ca49a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.1ca49a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5224417a77d410517d2c",
+    "url": "/reactapp/static/js/main.b35cee79.chunk.js"
+  },
+  {
+    "revision": "79fe4eb897a2ba752508",
+    "url": "/reactapp/static/js/runtime-main.ae977bfa.js"
+  }
+]);
