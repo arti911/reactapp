194b3695987335aeb1a1867905061b4a96a009bb
Artur <artur.jog@yandex.rum>
2020-12-11 16:41:26 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from "react";
-import CalculateBalance from "./components/CalculateBalance";
+import { Link, BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Layout, Menu } from "antd";
 
-const App = () => <CalculateBalance />;
+import WelcomeScreen from "./page/WelcomeScreen";
+
+const { Header, Content } = Layout;
+
+const App = () => {
+  return (
+    <Layout>
+      <Router>
+        <Header>
+          <Menu theme={"dark"} mode={"horizontal"}>
+            <Menu.Item key="app">
+              <Link to="/home">Home</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content>
+          <Route path="/home" component={WelcomeScreen} />
+          <Redirect from="/" to="/home" />
+        </Content>
+      </Router>
+    </Layout>
+  );
+};
 
 export default App;
