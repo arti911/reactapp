e8c72164dcf2cf3c1dea7adf6027b71abd547617
Artur Papikian <34072449+arti911@users.noreply.github.com>
2023-02-09 19:23:08 +0300

    refactor component Image

--- src/components/Image/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import IImage from "./index.interface";
-
-import "./style.scss";
-
-const Image = (props: IImage) => <img src={props.src} alt="car" style={{ width: "100%" }} />;
-
-export default Image;
