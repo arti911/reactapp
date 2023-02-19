478e2545cffc582e957200ade5fcb5f959ed7621
Artur Papikian <34072449+arti911@users.noreply.github.com>
2023-02-20 00:48:29 +0300

    added mocks

---
+++ src/components/Stories/data/index.ts
@@ -0,0 +1,410 @@
+import { IStoriesPreview } from "components/Stories/components/StoriesPreview";
+
+export const stories: IStoriesPreview[] = [
+  {
+    id: 2774873,
+    name: "Мотивация авторизовываться на сайте (2я волная)",
+    link: "https://lenta.com/npl/authentication/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/-2/.webp?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/-2/.webp?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/-2/.webp?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/-2/.webp?preset=mediumlossy",
+    },
+    buttonColor: "fe7203",
+    buttonTitle: "Войти",
+    contentItems: [
+      {
+        id: 2582788,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/-2/1.webp",
+      },
+      {
+        id: 2582789,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/-2/2.webp",
+      },
+      {
+        id: 2582790,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/-2/3.webp",
+      },
+      {
+        id: 2582791,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/-2/4.webp",
+      },
+      {
+        id: 2582792,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/-2/5.webp",
+      },
+    ],
+    openUrlViaBrowser: false,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2514225,
+    name: "Гид (лайфхаки)-unlim",
+    link: "https://smart.lenta.com/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/10.2022/800400px___-____.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/10.2022/800400px___-____.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/10.2022/800400px___-____.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/10.2022/800400px___-____.jpg?preset=mediumlossy",
+    },
+    buttonColor: "ffb900",
+    contentItems: [
+      {
+        id: 2514224,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/10.2022/10801920px___-___.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2757115,
+    name: "ЛН02 - 01.02.23",
+    link: "https://lenta.com/main/ln02/?utm_campaign=cnc_gm",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_2__.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_2__.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_2__.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_2__.jpg?preset=mediumlossy",
+    },
+    buttonColor: "ffb900",
+    contentItems: [
+      {
+        id: 2757113,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/10801920_2_--.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2758886,
+    name: "Harmony 21\\02(сайт)",
+    link: "https://lenta.com/goods-actions/harmony/?utm_campaign=cnc_gm",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400___.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400___.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400___.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400___.jpg?preset=mediumlossy",
+    },
+    buttonColor: "3d2555",
+    contentItems: [
+      {
+        id: 2758874,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/10801920___.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2756804,
+    name: "Есть всё! Для рассады (сайт) 30\\03",
+    link: "https://lenta.com/goods-actions/seedling_season/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/_800400____.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/_800400____.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/_800400____.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/_800400____.jpg?preset=mediumlossy",
+    },
+    buttonColor: "3a672c",
+    contentItems: [
+      {
+        id: 2754439,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/_10801920____.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2388447,
+    name: "tefal-28/02/23",
+    link: "https://fishki.lenta.com/?utm_source=stories&amp;utm_medium=_lentaappgiper&amp;utm_campaign=fishki",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400_new_tefal_fishki.png?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400_new_tefal_fishki.png?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400_new_tefal_fishki.png?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400_new_tefal_fishki.png?preset=mediumlossy",
+    },
+    buttonColor: "E3000B",
+    contentItems: [
+      {
+        id: 2388444,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/09.22/10801920px_tefal____-1.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2396672,
+    name: "Лента Открытий, Баллы за отзывы - 31.12.23",
+    link: "https://lenta.com/landings/lentaonly/?utm_source=stories&amp;utm_medium=_lentaappgiper&amp;utm_campaign=lentaonlyspbnsk",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400px__-1.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400px__-1.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400px__-1.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/09.22/800400px__-1.jpg?preset=mediumlossy",
+    },
+    buttonColor: "F8B024",
+    contentItems: [
+      {
+        id: 2396669,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/09.22/10801920px___1-1.jpg",
+      },
+      {
+        id: 2396670,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/09.22/10801920px___2-1.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2592111,
+    name: "Стали лучше Лента открытий(2)-31.12.23",
+    link: "https://lenta.com/landings/lentaonly/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/11.2022/800400px____.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/11.2022/800400px____.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/11.2022/800400px____.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/11.2022/800400px____.jpg?preset=mediumlossy",
+    },
+    buttonColor: "6e247d",
+    contentItems: [
+      {
+        id: 2591904,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/11.2022/1-1.jpg",
+      },
+      {
+        id: 2746436,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/10801920px___.jpg",
+      },
+      {
+        id: 2591903,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/11.2022/2-2.jpg",
+      },
+      {
+        id: 2591902,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/11.2022/3-1.jpg",
+      },
+      {
+        id: 2605517,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/11.2022/4-1.jpg",
+      },
+      {
+        id: 2605516,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/11.2022/5.jpg",
+      },
+      {
+        id: 2591901,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/11.2022/4.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2251252,
+    name: "все необходимое - 31.12.23",
+    link: "https://lenta.com/podborka/necessary-at-low-prices/?utm_source=stories&amp;utm_medium=_lentaappgiper&amp;utm_campaign=necessaryatlowprices2022",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/05.2022/800400.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/05.2022/800400.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/05.2022/800400.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/05.2022/800400.jpg?preset=mediumlossy",
+    },
+    buttonColor: "ffb900",
+    contentItems: [
+      {
+        id: 2251251,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/05.2022/10801920.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2761485,
+    name: "(сайт) Кешбэк для студентов - 31.03.23",
+    link: "https://lenta.com/goods-actions/students/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/__-2023-dg-800400.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/__-2023-dg-800400.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/__-2023-dg-800400.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/__-2023-dg-800400.jpg?preset=mediumlossy",
+    },
+    buttonColor: "fdbe0f",
+    contentItems: [
+      {
+        id: 2761461,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/__-2023-dg-10801920.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2767950,
+    name: "(Сайт) Сезон выгодных закупок - 07.02.23",
+    link: "https://lenta.com/goods-actions/benefit-season/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400__2__.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400__2__.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400__2__.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400__2__.jpg?preset=mediumlossy",
+    },
+    buttonColor: "b7182e",
+    contentItems: [
+      {
+        id: 2767941,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/10801920__2_.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2775090,
+    name: "(Сайт) Неделя красоты - 05.02.23",
+    link: "https://lenta.com/goods-actions/beauty_week/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/--800x400.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/--800x400.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/--800x400.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/--800x400.jpg?preset=mediumlossy",
+    },
+    buttonColor: "b7182e",
+    contentItems: [
+      {
+        id: 2775087,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/--1080x1920.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2775180,
+    name: "(Сайт) Есть всё! Для слад.моментов - 14.02.23",
+    link: "https://lenta.com/goods-actions/sweet_moments/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_-----dg6.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_-----dg6.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_-----dg6.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_-----dg6.jpg?preset=mediumlossy",
+    },
+    buttonColor: "003c96",
+    contentItems: [
+      {
+        id: 2775151,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/10801920_-----dg7.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+  {
+    id: 2777400,
+    name: "(Сайт) VK Mini app",
+    link: "https://lenta.com/goods-actions/vk-miniapp/",
+    previewImage: {
+      thumbnail:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_____.jpg?preset=thumbnail",
+      medium:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_____.jpg?preset=medium",
+      fullSize:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_____.jpg?preset=fullsize",
+      mediumLossy:
+        "https://lenta.servicecdn.ru/globalassets/stories/01-2023/800400_____.jpg?preset=mediumlossy",
+    },
+    buttonColor: "ffb900",
+    contentItems: [
+      {
+        id: 2777398,
+        type: "Image",
+        link: "https://lenta.servicecdn.ru/globalassets/stories/01-2023/10801920____2.jpg",
+      },
+    ],
+    openUrlViaBrowser: true,
+    announcementFullScreen: false,
+  },
+];
